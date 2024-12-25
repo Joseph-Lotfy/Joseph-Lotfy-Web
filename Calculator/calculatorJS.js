c13b9003cc75221dcc0c44f2670b00fb45d0abb8
Joseph-Lotfy <joseph418142@feng.bu.edu.eg>
2024-12-26 00:22:39 +0200

    Final commit fr

---
+++ Calculator/calculatorJS.js
@@ -0,0 +1,18 @@
+﻿// Function to append the value to the display
+function addValue(value) {
+    document.form1.answer.value += value;
+}
+
+// Function to calculate and display the result
+function calculateResult() {
+    try {
+        document.form1.answer.value = eval(document.form1.answer.value);
+    } catch (error) {
+        document.form1.answer.value = "Error";
+    }
+}
+
+// Function to clear the display
+function clearDisplay() {
+    document.form1.answer.value = '';
+}
